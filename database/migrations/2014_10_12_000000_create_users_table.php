a22f68c84d1e69e8a531806746e1ed8b2e1996a3
Fardin Ahsan <fardin.ahsan75@gmail.com>
2022-03-26 17:09:59 +0600

    User & ROle relationship

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,20 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->unique()->nullable();
             $table->string('email')->unique();
+            $table->string('phone')->unique()->nullable();
+            $table->enum('status', [1, 0])->default(1);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+            $table->foreignId('created_by')->nullable();
+            $table->timestamp('created_at')->useCurrent();
+            $table->foreignId('deleted_by')->nullable()->comment('User ID who deleted the data');
+            $table->timestamp('deleted_at')->nullable()->comment('When was the data deleted');
+
+            $table->foreign('created_by')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('deleted_by')->references('id')->on('users')->onUpdate('cascade')->onDelete('set null');
         });
     }
 
